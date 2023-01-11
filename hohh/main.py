75f8569f94e318df8347aa9cab6b93456cbc8da7
Riley Chase <riley.chase@rileychase.net>
2023-01-11 16:37:30 +1030

    Update CLI log to also filter from Spotify release dates

--- hohh/main.py
+++ hohh/main.py
@@ -1,22 +1,38 @@
 import argparse
 
+from hohh import spotify
+
 from . import config, lastfm
 
 
-def main():  # pylint: disable=missing-docstring
+def main():
+    # pylint: disable=missing-docstring, invalid-name
     args = setup_argparse().parse_args()
-    cfg = config.Config(username=args.username)
+    cfg = config.Config(lastfm_username=args.username)
+    client = spotify.SpotifyClient(
+        spotify_id=cfg.spotify_id, spotify_secret=cfg.spotify_secret
+    )
 
-    for track in lastfm.get_playback_history(cfg):
-        print(f"{track=}")
+    print("Title, Artist, Date")
+    for t in spotify.filter_tracks(cfg, client, lastfm.get_playback_history(cfg)):
+        print(f'"{t.title}", "{t.artist}", "{t.date}"')
 
 
 def setup_argparse() -> argparse.ArgumentParser:
     """Create an `argparse.ArgumentParser` to parse `hohh` CLI args."""
-    # TODO: Set app name correctly
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="hohh",
+        description=(
+            "Generate a list of tracks that you've heard that are eligible for this "
+            + "year's Triple J's Hottest 100.'"
+        ),
+    )
 
-    # TODO: Help/description strings
-    parser.add_argument("username", metavar="USER", type=str)
+    parser.add_argument(
+        "username",
+        metavar="USER",
+        type=str,
+        help="Your Last.fm username, to retrieve playback history.",
+    )
 
     return parser
