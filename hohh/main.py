c2851f564c55213b775891c9fc7f1e5b6f518917
Riley Chase <riley.chase@rileychase.net>
2023-01-11 14:59:05 +1030

    Implement initial CLI functionality

--- hohh/main.py
+++ hohh/main.py
@@ -1,2 +1,22 @@
-def main():
-    print("hello world")
+import argparse
+
+from . import config, lastfm
+
+
+def main():  # pylint: disable=missing-docstring
+    args = setup_argparse().parse_args()
+    cfg = config.Config(username=args.username)
+
+    for track in lastfm.get_playback_history(cfg):
+        print(f"{track=}")
+
+
+def setup_argparse() -> argparse.ArgumentParser:
+    """Create an `argparse.ArgumentParser` to parse `hohh` CLI args."""
+    # TODO: Set app name correctly
+    parser = argparse.ArgumentParser()
+
+    # TODO: Help/description strings
+    parser.add_argument("username", metavar="USER", type=str)
+
+    return parser
